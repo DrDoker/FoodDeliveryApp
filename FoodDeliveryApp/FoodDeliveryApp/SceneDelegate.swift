abc5fd7f9233068b71da2e76cff62953537d8c5f
Serhii Tkachenko <doker.tk@gmail.com>
2024-02-07 22:50:52 +0100

    Created based project and removed storyboard

---
+++ FoodDeliveryApp/FoodDeliveryApp/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  FoodDeliveryApp
+//
+//  Created by Serhii  on 07/02/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
